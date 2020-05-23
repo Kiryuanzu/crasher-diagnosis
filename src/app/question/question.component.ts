6fb36aa8550c0a49b6d47ca1faff226a5019ce14
kiryuanzu <anzu.nagato.1005@gmail.com>
2020-05-24 00:19:45 +0900

    質問をボタンで前後に遷移できるように記述した

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../question';
+import { QUESTIONS } from '../question-mock';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  questions = QUESTIONS;
+  constructor() {}
 
-  constructor() { }
-
+  currentQuestion: Question;
   ngOnInit(): void {
+    this.currentQuestion = this.questions[0];
   }
 
+  currentIndex() {
+    return this.questions.indexOf(this.currentQuestion);
+  }
+  next() {
+    this.currentQuestion = this.questions[this.currentIndex() + 1];
+  }
+
+  back() {
+    this.currentQuestion = this.questions[this.currentIndex() - 1];
+  }
 }
