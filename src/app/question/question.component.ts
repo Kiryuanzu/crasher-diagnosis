97b9f4db279e324f7d3808cc45adb42d8e231d4d
kiryuanzu <anzu.nagato.1005@gmail.com>
2020-05-30 01:57:40 +0900

    Qeustionコンポーネント内にResultのデータを表示できるようにした

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from '../question';
+import { Result } from '../result';
+import { RESULTS } from '../result-data';
 import { QUESTIONS } from '../question-data';
 import { FormControl, FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-question',
@@ -9,10 +12,18 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+  typeANum = 0;
+  typeBNum = 0;
+  typeCNum = 0;
+
+  resultOn: boolean;
   questions = QUESTIONS;
+  result: Result;
+  results = RESULTS;
   form = new FormGroup({
     point: new FormControl(''),
   });
+
   constructor() {}
 
   currentQuestion: Question;
@@ -23,16 +34,66 @@ export class QuestionComponent implements OnInit {
   currentIndex() {
     return this.questions.indexOf(this.currentQuestion);
   }
-  next() {
-    if (this.form.value.point == '') {
-      alert('選択肢が入力されていません');
-      return;
+
+  select(point: number) {
+    if (this.currentQuestion.type == 'A') {
+      this.typeANum += point;
+      console.log(this.typeANum);
+    } else if (this.currentQuestion.type == 'B') {
+      this.typeBNum += point;
+      console.log(this.typeBNum);
+    } else if (this.currentQuestion.type == 'C') {
+      this.typeCNum += point;
+      console.log(this.typeCNum);
     }
     this.currentQuestion = this.questions[this.currentIndex() + 1];
-    this.form.setValue({ point: '' });
-  }
-
-  back() {
-    this.currentQuestion = this.questions[this.currentIndex() - 1];
+    if (this.currentQuestion == this.questions[-1]) {
+      this.resultOn = true;
+      if (20 <= this.typeANum && 20 <= this.typeBNum && 20 <= this.typeCNum) {
+        this.result = this.results.find((result) => result.type == 'A');
+      } else if (
+        20 <= this.typeANum &&
+        20 <= this.typeBNum &&
+        20 >= this.typeCNum
+      ) {
+        this.result = this.results.find((result) => result.type == 'B');
+      } else if (
+        20 <= this.typeANum &&
+        this.typeBNum <= 20 &&
+        this.typeCNum <= 20
+      ) {
+        this.result = this.results.find((result) => result.type == 'C');
+      } else if (
+        this.typeANum <= 20 &&
+        20 <= this.typeBNum &&
+        this.typeCNum <= 20
+      ) {
+        this.result = this.results.find((result) => result.type == 'D');
+      } else if (
+        this.typeANum <= 20 &&
+        20 <= this.typeBNum &&
+        20 <= this.typeCNum
+      ) {
+        this.result = this.results.find((result) => result.type == 'E');
+      } else if (
+        this.typeANum <= 20 &&
+        this.typeBNum <= 20 &&
+        20 <= this.typeCNum
+      ) {
+        this.result = this.results.find((result) => result.type == 'F');
+      } else if (
+        20 <= this.typeANum &&
+        this.typeBNum <= 20 &&
+        20 <= this.typeCNum
+      ) {
+        this.result = this.results.find((result) => result.type == 'G');
+      } else if (
+        this.typeANum <= 20 &&
+        this.typeBNum <= 20 &&
+        this.typeCNum <= 20
+      ) {
+        this.result = this.results.find((result) => result.type == 'H');
+      }
+    }
   }
 }
