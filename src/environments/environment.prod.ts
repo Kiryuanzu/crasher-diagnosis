990aabdc34f8d80def282909e61db3dff588cc59
kiryuanzu <kyoka.fujiike@classi.jp>
2020-09-04 23:56:14 +0900

    environmentsに必要な情報を登録するため

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCZUpxDatVcr86OVlQYQtmBifzCktto_nw',
+    authDomain: 'circlecrusher-diagnosis.firebaseapp.com',
+    databaseURL: 'https://circlecrusher-diagnosis.firebaseio.com',
+    projectId: 'circlecrusher-diagnosis',
+    storageBucket: 'circlecrusher-diagnosis.appspot.com',
+    messagingSenderId: '158476269900',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//apiKeyはfirebase上でパブリック情報として扱われるので公開しても問題ないとのこと
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCZUpxDatVcr86OVlQYQtmBifzCktto_nw',
+    authDomain: 'circlecrusher-diagnosis.firebaseapp.com',
+    databaseURL: 'https://circlecrusher-diagnosis.firebaseio.com',
+    projectId: 'circlecrusher-diagnosis',
+    storageBucket: 'circlecrusher-diagnosis.appspot.com',
+    messagingSenderId: '158476269900',
+  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
